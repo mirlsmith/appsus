062ab559df5cec9b1c8dba53469f3efbbb6cf19c
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-09 13:11:48 +0200

    base header layout

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -1,11 +1,28 @@
+const navLinks = [
+    { to: '/mail', label: 'Mail' },
+    { to: '/keep', label: 'Keep' },
+    // { to: '/books', label: 'Books' },
+    { to: '/about', label: 'About' }
+]
+
 export default {
 	template: `
-        <header class="app-header">
-            <h1>AppSus</h1>
-            <nav>
-                <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link>
+        <header class="app-header main-layout full">
+            <nav class="main-nav">
+                <router-link to="/">
+                    <h1>AppSus</h1>
+                </router-link>
+                <ul class="clean-list flex">
+                    <router-link v-for="link in getLinks" :to="link.to" :key="link.label">
+                        <li>{{ link.label }}</li>
+                    </router-link>
+                </ul>
             </nav>
         </header>
     `,
+    computed: {
+        getLinks() {
+            return navLinks
+        }
+    }
 }
