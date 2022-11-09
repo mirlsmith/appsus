96701d6593c83da9249448bae914a23c9396a88e
Miriam Smith <miriamzeff@gmail.com>
2022-11-09 14:58:27 +0200

    fixed error message

---
+++ apps/mail/services/mail.service.js
@@ -0,0 +1,49 @@
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+import mailData from '../data/mails.json' assert {type: 'json'}
+
+
+const loggedInUser = {
+    email: 'user@appsus.com',
+    fullname: 'Merah Geshi'
+}
+
+const MAILS_KEY = 'mails'
+_createMails()
+
+export const mailService = {
+    query,
+    get,
+    remove,
+    save
+}
+
+function query() {
+    return storageService.query(MAILS_KEY)
+}
+
+function get(mailId){
+    return storageService.get(MAILS_KEY, mailId)
+}
+
+function remove(mailId) {
+    return storageService.remove(MAILS_KEY,mailId)
+}
+
+function save(mail) {
+    // if (mail.id) return storageService.put(MAILS_KEY, mail) //utilize for DRAFT feature?
+    // else 
+    return storageService.post(MAILS_KEY, mail)
+}
+
+
+function _createMails() {
+    let mails = utilService.loadFromStorage(MAILS_KEY)
+    if (!mails || !mails.length) {
+        mails = mailData
+
+        utilService.saveToStorage(MAILS_KEY, mails)
+    }
+    return mails
+}
--- views/app-mail.cmp.js
+++ views/app-mail.cmp.js
@@ -1,10 +1,32 @@
-export default {
-	template: `
-        <section class="mail-page">
-            <h1>Mail</h1>
-            <router-link to="/mail/details">details</router-link>
+import { mailService } from "../apps/mail/services/mail.service.js"
 
+export default {
+    template: `
+        <section class="mail-page">
+            <nav class="mail-sidenav">
+                <button>Compose</button>
+                <a href="#">Inbox</a>
+                <a href="#">Starred</a>
+                <a href="#">Sent Mail</a>
+                <a href="#">Drafts</a>
+            </nav>
+            <header class="mail-header">
+                <input type="search" placeholder="Search" />
+            </header>
+
+            <section class="mail-container">
+                <h1>Mail</h1>
+
+
+            </section>
+            
+            <!-- <router-link to="/mail/details">details</router-link> -->
             <router-view></router-view>
         </section>
     `,
+
+    components: {
+        mailService,
+    }
 }
+
