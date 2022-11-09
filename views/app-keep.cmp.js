d411355065d5cb70dddee74fa40e236385d6ca9d
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-09 11:02:21 +0200

    views

---
+++ views/app-keep.cmp.js
@@ -0,0 +1,7 @@
+export default {
+	template: `
+        <section class="keep-page">
+            <h1>Keep</h1>
+        </section>
+    `,
+}
---
+++ views/app-mail.cmp.js
@@ -0,0 +1,10 @@
+export default {
+	template: `
+        <section class="mail-page">
+            <h1>Mail</h1>
+            <router-link to="/mail/details">details</router-link>
+
+            <router-view></router-view>
+        </section>
+    `,
+}
