dfe372b0b1ee5fbb40ac0e03ffc614c62c48c89f
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-10 18:21:11 +0200

    layout side-nav

--- views/app-keep.cmp.js
+++ views/app-keep.cmp.js
@@ -5,11 +5,11 @@ import noteModal from '../apps/keep/cmps/note-modal.cmp.js'
 import searchBar from '../cmps/search-bar.cmp.js'
 
 const asideLinks = [
-    { to: '/keep', faClass: 'fa fa-sticky-note-o' },
-    { to: '/keep/text', faClass: 'fa fa-font' },
-    { to: '/keep/video', faClass: 'fa fa-video-camera' },
-    { to: '/keep/image', faClass: 'fa fa-picture-o' },
-    { to: '/keep/todos', faClass: 'fa fa-list' }
+    { to: '/keep', faClass: 'fa fa-sticky-note-o', title: 'Keep' },
+    { to: '/keep/text', faClass: 'fa fa-font', title: 'Texts' },
+    { to: '/keep/video', faClass: 'fa fa-video-camera', title: 'Videos' },
+    { to: '/keep/image', faClass: 'fa fa-picture-o', title: 'Images' },
+    { to: '/keep/todos', faClass: 'fa fa-list', title: 'Todos' }
 ]
 
 const gFilterMapKey = {
@@ -26,13 +26,15 @@ export default {
 
             <search-bar @searched="setFilterByTxt" />
 
-            <aside class="note-filters">
-                <router-link v-for="link in getLinks" :to="link.to" :key="link.label">
-                    <i :class="link.faClass"></i>
+            <aside class="side-nav">
+                <router-link v-for="link in getLinks" :to="link.to" :key="link.title">
+                    <i :class="link.faClass" :title="link.title"></i>
                 </router-link>
             </aside>
 
             <div class="notes-container">
+
+                <p v-if="pinnedNotesToShow.length > 0">PINNED</p>
                 <note-list :notes="pinnedNotesToShow"
                     enter-class="animate__bounceIn"
                     leave-class="animate__bounceOut"
@@ -41,8 +43,7 @@ export default {
                     @onNoteRemove="handleNoteRemove"
                     @onNoteDuplicate="handleNoteDuplicate" />
 
-                <hr>
-
+                <p v-if="notesToShow.length > 0">OTHERS</p>
                 <note-list :notes="notesToShow"
                     enter-class="animate__fadeInDown"
                     leave-class="animate__backOutDown"
@@ -50,6 +51,7 @@ export default {
                     @onNotePinned="handleNotePinned"
                     @onNoteRemove="handleNoteRemove"
                     @onNoteDuplicate="handleNoteDuplicate" />
+
             </div>
 
             <div v-if="isNoteModalOpen" class="backdrop" @click.self="this.isNoteModalOpen = false"></div>
