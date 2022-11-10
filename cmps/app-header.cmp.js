83f278a9342794f21ea8a1905ffeaa8b45ed7908
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-10 21:08:36 +0200

    relate to prev commit

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -11,7 +11,7 @@ export default {
         <header class="app-header full">
             <nav class="main-nav">
                 <router-link to="/">
-                    <img src="../assets/img/home.png" alt="App Icon">
+                    <img src="assets/img/home.png" alt="App Icon">
                 </router-link>
                 <div class="main-links">
                     <svg @click="isMenuOpen = !isMenuOpen" focusable="false" viewBox="0 0 24 24"><path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
