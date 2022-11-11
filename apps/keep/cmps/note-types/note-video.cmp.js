6f564878fd49452bbca78e8298376b2ba9dc9243
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-11 12:55:20 +0200

    add title

--- apps/keep/cmps/note-types/note-video.cmp.js
+++ apps/keep/cmps/note-types/note-video.cmp.js
@@ -1,6 +1,9 @@
 export default {
   props: ['info'],
   template: `
-    <iframe :src="info.url" width="100%"></iframe>
+    <div>
+      <h4 style="text-align: center; margin-bottom: 3px">{{ info.title }}</h4>
+      <iframe :src="info.url" width="100%"></iframe>
+    </div>
   `
 }
