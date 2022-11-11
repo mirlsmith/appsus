85b40cd3c8870ab3e445d6d608224e8adb202588
Miriam Smith <miriamzeff@gmail.com>
2022-11-11 16:20:27 +0200

    changed isRemoved to isDiscarded

--- apps/mail/cmps/mail-compose.cmp.js
+++ apps/mail/cmps/mail-compose.cmp.js
@@ -5,17 +5,17 @@ export default {
             <section class="mail-compose">
                 <div class="header">
                     <span class="new-msg">New Message</span>
-                    <button @click="mailSavedDraft" class="close" title="close and save">x</button>
+                    <button @click="mailSavedDraft" class="close" title="Close and save">x</button>
                 </div>
                 <form>
                     <input v-model.trim="mail.to" ref="recipient" type="text" class="mail-to" placeholder="To"/>
                     <input v-model.trim="mail.subject" type="text" class="mail-subject" placeholder="Subject"/>
                     <textarea v-model="mail.body" class="mail-body" cols="30" rows="10"></textarea>
                     <div class="actions">
-                        <button @click.prevent="mailSent" class="mail-send blue-mail-btn" title="send">Send</button>
+                        <button @click.prevent="mailSent" class="mail-send blue-mail-btn" title="Send">Send</button>
                         <i 
                         @click.prevent="mailDiscard"
-                        class="fa-solid fa-trash-can clk" title="discard"></i>
+                        class="fa-solid fa-trash-can clk" title="Discard"></i>
                     </div>
                 </form>
             </section>
@@ -30,7 +30,7 @@ export default {
                 sentTimeStamp: Date.now()/1000,
                 body: '',
                 isStarred: false,
-                isRemoved: false,
+                isDiscarded: false,
                 isRead: false,
                 isDraft: true,
                 labels: ['','']
