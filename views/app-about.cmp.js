c89c82da212ba18804743c37a6f228d19b6701b8
Miriam Smith <miriamzeff@gmail.com>
2022-11-12 20:55:13 +0200

    created about page

--- views/app-about.cmp.js
+++ views/app-about.cmp.js
@@ -1,7 +1,50 @@
 export default {
 	template: `
         <section class="about-page">
-            <h1>About us</h1>
+            <h1>Meet the Team</h1>
+            <section class="team-members">
+
+            <div class="team-card">
+                <div class="img-wrapper moshe"></div>
+                <h2 class="team-name">Moshe Nehemiah
+                </h2>
+                <div class="team-bio">
+                    <p> Moshe Nehemiah is new to the fullstack industry but 
+                        his bright mind and eagerness to learn 
+                        will make him a truly valuable asset to any team.</p>
+                    <p> When he isn't writing code you'll find him hunkered down playing his favorite game - Apex Legends.</p>
+                </div>
+                <div class="social-media-links">
+                    <a href="https://github.com/Y-Moshe" target="_blank">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/moshe-nehemiah-254506155/" target="_blank">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="team-card">
+                <div class="img-wrapper miriam"></div>
+                <h2 class="team-name">Miriam Smith
+                </h2>
+                <div class="team-bio">
+                    <p> Miriam Smith is ready to take the plunge and dive into the world of fullstack.
+                        Her many years of experience as a production manager, her eagerness to learn
+                        and her excellent analysis skills make her a great addition to any team of developers.
+                    </p>
+                    <p> When she isn't writing code you'll find her watching movies with her husband and four kids.</p>
+                </div>
+                <div class="social-media-links">
+                    <a href="https://github.com/mirlsmith" target="_blank">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/miriam-smith-462842119/" target="_blank">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+            </section>
         </section>
     `
 }
