34caefdc84fe28c63ca4c13e9d4b22d31d44aa9b
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-13 01:19:31 +0200

    overflow hidden on note-preview

--- apps/keep/cmps/note-types/note-txt.cmp.js
+++ apps/keep/cmps/note-types/note-txt.cmp.js
@@ -5,7 +5,7 @@ export default {
   template: `
     <div @click.stop="">
       <h4 contenteditable="true" @input="onTxtChange($event, 'title')" class="outline-none">{{ info.title }}</h4>
-      <p contenteditable="true" @input="onTxtChange($event, 'txt')" class="outline-none">{{ info.txt }}</p>
+      <p contenteditable="true" @input="onTxtChange($event, 'txt')" class="outline-none" style="white-space: normal">{{ info.txt }}</p>
     </div>
   `,
   methods: {
