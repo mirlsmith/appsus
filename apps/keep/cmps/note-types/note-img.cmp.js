a4b8126ef100ca04abf5f20e9a7c6fff4256b5cc
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-16 00:26:11 +0200

    center img

--- apps/keep/cmps/note-types/note-img.cmp.js
+++ apps/keep/cmps/note-types/note-img.cmp.js
@@ -1,6 +1,6 @@
 export default {
   props: ['info'],
   template: `
-    <img :src="info.url" :alt="info.title" />
+    <img :src="info.url" :alt="info.title" style="display: inline-block; margin: auto" />
   `
 }
